b1c4adf965e4270280a069f6a9c2102013e70315
passion <563129582@qq.com>
2024-07-15 00:13:22 +0800

    修改menu，添加提示

--- Assets/Scripts/MenuPanel.cs
+++ Assets/Scripts/MenuPanel.cs
@@ -7,10 +7,17 @@ using UnityEngine.UI;
 public class MenuPanel : MonoBehaviour
 {
     public Button m_LoginBtn;
+    public Button m_QuitBtn;
     
     void Start()
     {
         m_LoginBtn.onClick.AddListener(OnLoginClick);
+        m_QuitBtn.onClick.AddListener(OnQuitClick);
+    }
+
+    private void OnQuitClick()
+    {
+        Application.Quit();
     }
 
     private void OnLoginClick()
